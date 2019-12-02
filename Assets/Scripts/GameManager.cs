f7822d2246ab834636bd2f35b3b71eecafad5f84
Jordan Wilson <46056108+sirdreadalot@users.noreply.github.com>
2019-12-02 17:11:44 +0000

    Added Menus
    Add the following:
    -Main Menu with working button
    -On death event for the hero to change the scene to a game over one
    -Added a game over screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Space Shooter BANG");
+    }
+    public void EndGame()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
