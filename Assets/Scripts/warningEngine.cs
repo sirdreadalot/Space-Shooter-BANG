01b2366c04a6f992caa1d3bd39f3d70947570a8a
Jordan Wilson <46056108+sirdreadalot@users.noreply.github.com>
2019-12-09 17:14:26 +0000

    Update 4.4
    Added a 'Warning border', meaning the screen will flash red and produce a timer which once at 0 will kill the player

---
+++ Assets/Scripts/warningEngine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class warningEngine : MonoBehaviour
+{
+    public static warningEngine instance;
+
+    public bool trigger = false;
+
+    public float timer = 10f;
+
+    public Text timerText;
+
+    public GameObject uiArt;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (trigger)
+        {
+            timer -= Time.deltaTime;
+            timerText.text = timer.ToString("F1"); 
+
+            if(timer <= 0)
+            {
+                print("Dead");
+                trigger = false;
+            }
+        }  
+    }
+
+    public void enterArea()
+    {
+        uiArt.SetActive(true);
+        trigger = true;
+    }
+
+    public void exitArea()
+    {
+        trigger = false;
+        uiArt.SetActive(false);
+        timer = 10f;
+    }
+}
---
+++ Assets/borderAreaTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class borderAreaTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D hit)
+    {
+        warningEngine.instance.enterArea(); 
+    }
+
+    private void OnTriggerExit2D(Collider2D hit)
+    {
+        warningEngine.instance.exitArea();
+    }
+
+}
