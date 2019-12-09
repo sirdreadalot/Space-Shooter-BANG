67684d277bf7e483ffef8126e469e19445ee057c
Jordan Wilson <46056108+sirdreadalot@users.noreply.github.com>
2019-12-09 15:56:55 +0000

    Update 4.3
    Added 'rocket' to the player, along with a small animation to when things die

---
+++ Assets/Scripts/Border.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    private void collider2D(BoxCollider2D collision)
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        x = 0;
+        y = 0;
+    }
+
+
+
+
+
+
+    // Start is called before the first frame update
+}
+
