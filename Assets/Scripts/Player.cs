c8459681f32cf66fa18a22a872155a232fa5e66a
Jordan Wilson <46056108+sirdreadalot@users.noreply.github.com>
2020-01-04 16:45:14 +0000

    Update 3.0
    -Got rid of the warning system
    -Added blocks to the player movement on the borders of the screen as to limit movement
    -Fixed the game's resolution to 1024x768

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,6 @@ public class Player : MonoBehaviour
     public delegate void UpdateHealth(int newHealth);
     public static event UpdateHealth OnUpdateHealth;
 
-    private Animator gunAnim;
-    private void Start()
-    {
-        gunAnim = GetComponent<Animator>();
-    }
-    private void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            gunAnim.SetBool("isFiring", true);
-        }
-        else
-        {
-            gunAnim.SetBool("isFiring", false);
-        }
-    }
     public void SendHealthData(int health)
     {
         if (OnUpdateHealth != null)
--- Assets/Scripts/TopDownCharacterController2D.cs
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -14,5 +14,24 @@ public class TopDownCharacterController2D : MonoBehaviour
         float y = Input.GetAxis("Vertical");
         rigidbody2D.velocity = new Vector2(x, y) * speed;
         rigidbody2D.angularVelocity = 0.0f;
+        //X axis
+        if (transform.position.x <= -13.84f)
+        {
+            transform.position = new Vector2(-13.84f, transform.position.y);
+        }
+        else if (transform.position.x >= 13.84f)
+        {
+            transform.position = new Vector2(13.84f, transform.position.y);
+        }
+
+        //Y axis
+        if (transform.position.y <= -10.15f)
+        {
+            transform.position = new Vector2(transform.position.x, -10.15f);
+        }
+        else if (transform.position.y >= 10.15f)
+        {
+            transform.position = new Vector2(transform.position.x, 10.15f);
+        }
     }
 }
